2523892a5cb499c68b7b7b25e7e939402a9b1b3a
Sahar-Mgh <sahar.emma.m@gmail.com>
2024-01-10 17:56:59 +0330

    added the HTML and CSS code

---
+++ script.js
@@ -0,0 +1,10 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "123456789";;
+const symbols = "~!@#$%^&*()_-+={[}]|;<,>.?/";
+
+const alChars = upperCase+lowerCase+numbers+symbols;
+
