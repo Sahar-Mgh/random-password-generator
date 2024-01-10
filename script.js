7430f2a45b1f3d7aa37278c4906bf283a5070b3a
Sahar-Mgh <sahar.emma.m@gmail.com>
2024-01-10 17:57:23 +0330

    created the generate password function

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ const symbols = "~!@#$%^&*()_-+={[}]|;<,>.?/";
 
 const alChars = upperCase+lowerCase+numbers+symbols;
 
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+    while(length > password.length){
+        password += alChars[Math.floor(Math.random() * alChars.length)];
+    }
+    passwordBox.value = password;
+}
